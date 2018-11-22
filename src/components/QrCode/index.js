030944a31a83f3c27dc2e59946af5e9566958811
vo.hong.huu <vo.hong.huu@framgia.com>
2018-11-22 11:45:14 +0700

    cameraandqrcode

---
+++ src/components/QrCode/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import Webcam from "react-webcam";
+
+
+class CallQrCode extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+      }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+    )   
+    }
+}
+export default CallQrCode;
